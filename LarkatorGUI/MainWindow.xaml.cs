772e0d516f8928dfd7f229a6ef87d344e54a2d91
jamckee <dj32mckee@hotmail.com>
2019-05-08 15:53:40 -0600

    Added duplicate list checking/Fixed adding new list names

--- LarkatorGUI/MainWindow.xaml.cs
+++ LarkatorGUI/MainWindow.xaml.cs
@@ -477,7 +477,6 @@ namespace LarkatorGUI
 
             speciesCombo.ItemsSource = arkReader.AllSpecies;
             groupsCombo.ItemsSource = ListSearches.Select(sc => sc.Group).Distinct().OrderBy(g => g).ToArray();
-            int temp = Properties.Settings.Default.lastGroup;
             groupsCombo.SelectedIndex = Properties.Settings.Default.lastGroup;
         }
 
@@ -528,12 +527,16 @@ namespace LarkatorGUI
 
         private void SaveSearch_Click(object sender, RoutedEventArgs e)
         {
-            List<String> NewSearchList;
-            SearchCriteria tempSearch;
             if (String.IsNullOrWhiteSpace(NewSearch.Species)) return;
+
+            List<String> NewSearchList = new List<String>(AllSpecies.Where(species => species.Contains(NewSearch.Species)));
+            SearchCriteria tempSearch;
+            int order = 100;
+
             Properties.Settings.Default.lastGroup = groupsCombo.SelectedIndex;
             Properties.Settings.Default.Save();
-            NewSearchList = new List<String>(AllSpecies.Where(species => species.Contains(NewSearch.Species)));
+            
+
             if (NewSearchList.Count == 0) // No matches
             { //Trigger default values so the user knows we tried to match
                 NewSearch = null;
@@ -546,12 +549,19 @@ namespace LarkatorGUI
             try
             {
                 ObservableCollection<SearchCriteria> tempListSearch = new ObservableCollection<SearchCriteria>(ListSearches.Where(sc => sc.Group == (String) groupsCombo.SelectedValue));
+                if (tempListSearch.Count > 0)
+                {
+                    order = (int) ListSearches.Where(sc => sc.Group == NewSearch.Group).Max(sc => sc.Order) + 100;
+                }
                 foreach (String newDino in NewSearchList)
                 {
-                    tempSearch = new SearchCriteria(NewSearch);
-                    tempSearch.Species = newDino;
-                    tempSearch.Order = ListSearches.Where(sc => sc.Group == tempSearch.Group).Max(sc => sc.Order) + 100;
-                    ListSearches.Add(tempSearch);
+                    if (tempListSearch.Count == 0 || tempListSearch.Where(dino =>dino.Species == newDino).Count() == 0) {
+                        tempSearch = new SearchCriteria(NewSearch);
+                        tempSearch.Species = newDino;
+                        tempSearch.Order = order;
+                        ListSearches.Add(tempSearch);
+                        order += 100;
+                    }
                 }
             }
             catch (InvalidOperationException) // no entries for .Max - ignore
