d567aa47d1b47349204081fc0abbf379310b3d73
coldino <28345893+coldino@users.noreply.github.com>
2021-04-02 19:52:36 +0100

    Publish v1.10
    * Added the Genesis map (first one)
    * Added the Valguero map (Closes #39 - thanks @Rottenbeer)
    * Added the Crystal Isles map (thanks @m-soltys)
    * Recalibrated **all** the maps using a new calibration system (Fixes #35)
    * Updated ArkSavegameToolkit (a longshot that might help #47 or #51)
    * Added button to force a Refresh of the savegame - off by default (from #33, thanks @jamckee)
    * Added options to change which stats are displayed (Fix #31, from #33, thanks @jamckee)
    * Added support for partial species name match (from #33, thanks @jamckee)
    * Added option to hide untameable species (Fix #30, from #33, thanks @jamckee)
    * Fix teleport coordinates when in a locale that switches , and . (Fix #48, closes #40 - thanks @WiredSharp)
    * Never show the hover skiff "species"
    * Fixed a rare crash when reordering searches with drag&drop
    * Beef up error handling when accessing the clipboard (Fixes #45)
    * Allow search gender to be changed with click and add a tooltip for how to change levels
    * Introduce a new calibration system based on placed storage boxes in the world (see https://github.com/coldino/Larkator/wiki/Calibrating-New-Maps for more info)
    * Add protection against corrupted config files (Fixes #46)
    * Improve the savegame file watcher to hopefully never, ever need the refresh button
    
    Closes #50, Closes #52.

--- LarkatorGUI/Properties/AssemblyInfo.cs
+++ LarkatorGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LarkatorGUI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
