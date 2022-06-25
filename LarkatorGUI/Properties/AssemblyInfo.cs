ee6f66bd4115107f080f169756f3220ce2f3f084
coldino <28345893+coldino@users.noreply.github.com>
2022-06-25 10:45:23 +0100

    Add Fjordur map
    Be warned, the in-game drawn map wasn't updated to match the changes WC made to the coastline :(

--- LarkatorGUI/Properties/AssemblyInfo.cs
+++ LarkatorGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LarkatorGUI")]
-[assembly: AssemblyCopyright("Copyright © 2017-2021")]
+[assembly: AssemblyCopyright("Copyright © 2017-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
